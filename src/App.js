99465a25a541079352075966e9675ebb28028dbf
Rudra <rudra4365@gmail.com>
2021-04-23 00:51:06 +0530

    added delete

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, {useState, useEffect} from 'react';
 import './App.css';
 import {Button, FormControl, Input, InputLabel} from '@material-ui/core/';
-import Todo from './Todo'
-import db from './firebase'
+import Todo from './Todo';
+import db from './firebase';
+import firebase from 'firebase';
 
 function App() {
 
@@ -11,19 +12,27 @@ function App() {
 
   // When the application reloads, we need to listen to database and fetch
   // new todos as they get added or removed
-  // useEffect(() => {
-  //   setTodos()
-  // }, []);
+  useEffect(() => {
+    db.collection('todos').orderBy('timestamp', 'desc').onSnapshot(snapshot => {
+      setTodos(snapshot.docs.map(doc => ({id: doc.id ,todo: doc.data().todo}))) // Fetching data from firebase database
+    })
+  }, []);
 
   const addTodo = (event) => {
-    event.preventDefault();
-    setTodos([...todos, input]);
-    setInput(""); 
+    event.preventDefault(); //stop from reloading
+
+    db.collection('todos').add({ // appending in the firebase database
+      todo: input,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp()
+    })
+
+    // setTodos([...todos, input]); // append input at end of todos array
+    setInput("");  // empty the input
   }
 
   return (
     <div className="App">
-      <h1>Hello aaRPee... Let's take down your tasks :) </h1> 
+      <h1>{"Hello aaRPee... Let's take down your tasks 😃"} </h1> 
       <form>
       <FormControl>
         <InputLabel>Note down a task</InputLabel>
@@ -32,11 +41,11 @@ function App() {
 
       
         <Button disabled = {!input} type = "submit" variant="contained" color="primary" onClick = {addTodo}>
-          Add Todo
+          {"Add Todo"}
         </Button>
         <ul>
           {todos.map(todo => (
-            <Todo text = {todo} />
+            <Todo todo = {todo} />
           ))}
         </ul>
         </form>
--- src/Todo.js
+++ src/Todo.js
@@ -1,13 +1,17 @@
 import React from 'react'
-import {List, ListItem, ListItemText, ListItemAvatar} from '@material-ui/core/';
+import {List, ListItem, ListItemText, ListItemAvatar, Button} from '@material-ui/core/';
+import db from './firebase';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 function Todo(props) {
+
     return (
         <List className = "todo_list">
             <ListItem>
             <ListItemAvatar></ListItemAvatar>
-                <ListItemText secondary = "Deadline: " primary = {props.text} />
+                <ListItemText secondary = "Deadline: " primary = {props.todo.todo} />
             </ListItem>
+            <Button onClick = {event => db.collection('todos').doc(props.todo.id).delete()}><DeleteIcon /></Button>
         </List>
  )
 }
